dafa0316582cd60c6b7fd7d8133827396c8b1ecf
Raj Patel <patelraj24365@gmail.com>
2024-07-07 10:27:47 -0400

    create TileContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { PathfindingProvider } from './context/PathfindingContext'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { PathfindingProvider } from "./context/PathfindingContext";
+import { TileProvider } from "./context/TIleContext";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <PathfindingProvider>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <TileProvider>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </TileProvider>
     </PathfindingProvider>
-  )
+  );
 }
 
-export default App
+export default App;
