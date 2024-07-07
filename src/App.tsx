6bf0c11fd24170694838afc3d64e590fb8eb53d4
Raj Patel <patelraj24365@gmail.com>
2024-07-07 10:32:02 -0400

    create SpeedContext

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { PathfindingProvider } from "./context/PathfindingContext";
 import { TileProvider } from "./context/TIleContext";
+import { SpeedProvider } from "./context/SpeedContext";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +11,9 @@ function App() {
   return (
     <PathfindingProvider>
       <TileProvider>
+        <SpeedProvider>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </SpeedProvider>
       </TileProvider>
     </PathfindingProvider>
   );
